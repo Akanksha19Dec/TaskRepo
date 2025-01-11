2f4096995d446a14694f0e269bfe6d123e06b6a7
Akanksha Tallapalli <150659307+Akanksha19Dec@users.noreply.github.com>
2025-01-11 08:06:10 +0530

    Add files via upload

---
+++ Lab011.java
@@ -0,0 +1,14 @@
+//        Create a Function of Sub, Sum, Mul and Div ,Mod with parameter, a, b (take the parameter from the User)
+
+import java.util.Scanner;
+class Lab011 {
+
+public static void main (String [] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number ");
+int a = sc.nextInt();
+
+}
+}
+
